f2a40ec8c5f01fe572f32df8cea1570816e839a1
Benoit <benoit.theunissen@gmail.com>
2016-04-23 12:26:00 +0200

    fix equals and hash code for DefaultTestCase

--- settesting/src/main/java/com/github/btheu/settesting/core/impl/DefaultTestCase.java
+++ settesting/src/main/java/com/github/btheu/settesting/core/impl/DefaultTestCase.java
@@ -1,5 +1,8 @@
 package com.github.btheu.settesting.core.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.github.btheu.settesting.TestCase;
 import com.github.btheu.settesting.TestInput;
 
@@ -10,10 +13,12 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode
 public class DefaultTestCase implements TestCase {
 
-    private TestInput[] inputs;
+    protected List<Class<? extends TestInput>> inputClasess = new ArrayList<Class<? extends TestInput>>();
 
     public DefaultTestCase(TestInput...inputs) {
-        this.inputs = inputs;
+        for (TestInput testInput : inputs) {
+            this.inputClasess.add(testInput.getClass());
+        }
     }
 
 }
