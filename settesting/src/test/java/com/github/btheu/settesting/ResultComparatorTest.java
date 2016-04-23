f4c3f469e87752d0cb801e5d2b09c81adebb4a98
Benoit <benoit.theunissen@gmail.com>
2016-04-23 12:45:01 +0200

    remove ResultComparatorTest2.java

--- settesting/src/test/java/com/github/btheu/settesting/ResultComparatorTest.java
+++ settesting/src/test/java/com/github/btheu/settesting/ResultComparatorTest.java
@@ -1,68 +1,34 @@
 package com.github.btheu.settesting;
 
-import java.util.List;
-
 import org.junit.Test;
 
-import com.github.btheu.settesting.core.ReportLine;
-import com.github.btheu.settesting.core.ResultReport;
-import com.github.btheu.settesting.core.impl.DefaultResultComparator;
-import com.github.btheu.settesting.core.impl.DefaultTestCase;
-import com.github.btheu.settesting.core.impl.InMemoryGridResultProvider;
-import com.github.btheu.settesting.core.impl.InMemoryResultReport;
 import com.github.btheu.settesting.core.impl.PrimitiveValueResult;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ResultComparatorTest {
+public class ResultComparatorTest extends SetTestingCase {
 
-    @Test
-    public void test() throws InstantiationException, IllegalAccessException {
-        
-        DefaultResultComparator comp = new DefaultResultComparator();
-        InMemoryGridResultProvider gridResultProvider = new InMemoryGridResultProvider();
-        InMemoryResultReport report = new InMemoryResultReport();
-        
-        comp.setGridResultProvider(gridResultProvider);
-        comp.setResultReport(report);
-        
-        Class<SimpleUseCase> useCaseClass = SimpleUseCase.class;
-        
-        Class<SimpleObject> businessObjectClass = SimpleObject.class;
-        
-        SimpleUseCase useCase = useCaseClass.newInstance();
-        
-        SimpleObject businessObject = businessObjectClass.newInstance();
-        
-        Result r1 = new PrimitiveValueResult(1);
-        
-        Result r2 = new PrimitiveValueResult(2);
-        
-        Result r3 = new PrimitiveValueResult(3);
-        
-        comp.compare(r1,new DefaultTestCase(useCase, businessObject));
-        comp.compare(r1,new DefaultTestCase(useCase, businessObject));
-        comp.compare(r2,new DefaultTestCase(businessObject, useCase));
-        comp.compare(r2,new DefaultTestCase(businessObject, useCase));
-        
-        
-        comp.compare(r3,new DefaultTestCase(useCase, businessObject));
-        comp.compare(r3,new DefaultTestCase(businessObject, useCase));
-        
-        
-        ResultReport resultReport = comp.getReport();
-        
-        List<ReportLine> lines = resultReport.getAllLines();
-        for (ReportLine line : lines) {
-            log.info("{}",line.success());
-        }
+    @Test 
+    public void test2() {
+        this.usecases.add(SimpleUseCase1.class);
+        this.bos.add(SimpleObject.class);
+        this.bos.add(SimpleObject.class);
+        this.bos.add(SimpleObject.class);
     }
  
-    static class SimpleUseCase implements UseCase {
+    static class SimpleUseCase1 implements UseCase {
 
         public Result execute() {
-            return null;
+            return new PrimitiveValueResult(1);
+        }
+        
+    }
+    
+    static class SimpleUseCase2 implements UseCase {
+        
+        public Result execute() {
+            return new PrimitiveValueResult(1);
         }
         
     }
